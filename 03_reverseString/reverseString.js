92babe50b50010f01e8d1a8b5d3fb5352d81a900
King-Windows <krsazon@gmail.com>
2023-06-24 01:29:58 -0700

    Answers Exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
 const reverseString = function() {
-
+    let String = "";
+    let ReverseString = String.split("").reverse().join("");
+    
+    return ReverseString;
+    
 };
+
+console.log(reverseString());
 
 // Do not edit below this line
 module.exports = reverseString;
